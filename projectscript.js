eec65e2f4a54c15e563e97bf07a3e9232e76e702
ryalay <ryanlaytn@gmail.com>
2023-10-18 16:44:26 -0400

    all responsive bruh

--- projectscript.js
+++ projectscript.js
@@ -25,3 +25,28 @@ document.onreadystatechange = function () {
     }
 };
 
+const menu = document.querySelector(".mobilemenuopen");
+const menuItems = document.querySelectorAll(".menuItem");
+const mobilemenu= document.querySelector(".mobilemenu");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+mobilemenu.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+    function(menuItem) { 
+      menuItem.addEventListener("click", toggleMenu);
+    }
+  )
